b9e5d79247219b7ef43138f0f1233ab6ae4387d0
Khoi Pham <pckhoi@gmail.com>
2021-03-31 17:54:25 +0700

    Change path in tar file

--- scripts/build_binaries.sh
+++ scripts/build_binaries.sh
@@ -2,14 +2,17 @@
 set -ex
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
-cd $DIR
 
+cd $DIR
 rm -rf build
 
 CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -a -o build/wrgl-linux-amd64/bin/wrgl github.com/wrgl/core/wrgl
 cp LICENSE build/wrgl-linux-amd64/LICENSE
-tar -czvf build/wrgl-linux-amd64.tar.gz build/wrgl-linux-amd64
+cd $DIR/build
+tar -czvf wrgl-linux-amd64.tar.gz wrgl-linux-amd64
 
+cd $DIR
 CGO_ENABLED=0 GOARCH=amd64 GOOS=darwin go build -a -o build/wrgl-darwin-amd64/bin/wrgl github.com/wrgl/core/wrgl
 cp LICENSE build/wrgl-darwin-amd64/LICENSE
-tar -czvf build/wrgl-darwin-amd64.tar.gz build/wrgl-darwin-amd64
+cd $DIR/build
+tar -czvf wrgl-darwin-amd64.tar.gz wrgl-darwin-amd64
