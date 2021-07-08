3ada5773ce649a18a82957d9ad4a52b220a1788e
Khoi Pham <pckhoi@gmail.com>
2021-07-08 17:06:28 +0700

    add missing import

--- pkg/objects/str_list_test.go
+++ pkg/objects/str_list_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/wrgl/core/pkg/testutils"
 )
 
 func repeatStrSlice(sl []string, n int) []string {
