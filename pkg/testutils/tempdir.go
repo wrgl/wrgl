a55d79a009cd56b1d74af6f07d35abfb30d8babe
Khoi Pham <pckhoi@gmail.com>
2021-10-06 09:49:10 +0700

    fix: don't join dir with RUNNER_TEMP if dir starts with /

--- pkg/testutils/tempdir.go
+++ pkg/testutils/tempdir.go
@@ -4,13 +4,14 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 // TempDir is a light wrapper of ioutil.Tempdir which place the dir under
 // RUNNER_TEMP env var if it is specified. This is necessary for Github action
 // to work correctly.
 func TempDir(dir, pattern string) (string, error) {
-	if v := os.Getenv("RUNNER_TEMP"); v != "" {
+	if v := os.Getenv("RUNNER_TEMP"); v != "" && !strings.HasPrefix(dir, "/") {
 		dir = filepath.Join(v, dir)
 	}
 	return ioutil.TempDir(dir, pattern)
@@ -20,7 +21,7 @@ func TempDir(dir, pattern string) (string, error) {
 // RUNNER_TEMP env var if it is specified. This is necessary for Github action
 // to work correctly.
 func TempFile(dir, pattern string) (*os.File, error) {
-	if v := os.Getenv("RUNNER_TEMP"); v != "" {
+	if v := os.Getenv("RUNNER_TEMP"); v != "" && !strings.HasPrefix(dir, "/") {
 		dir = filepath.Join(v, dir)
 	}
 	return ioutil.TempFile(dir, pattern)
