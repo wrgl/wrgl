654d7b0e2cccc0f5811a74608b5f9e38d5cc7315
Khoi Pham <pckhoi@gmail.com>
2022-03-29 09:26:22 +0700

    feat: command authenticate works with oauth2

--- cmd/wrgl/credentials/authenticate_cmd.go
+++ cmd/wrgl/credentials/authenticate_cmd.go
@@ -6,15 +6,18 @@ package credentials
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/wrgl/wrgl/cmd/wrgl/utils"
+	"github.com/wrgl/wrgl/pkg/conf"
 	conffs "github.com/wrgl/wrgl/pkg/conf/fs"
 	"github.com/wrgl/wrgl/pkg/credentials"
 )
@@ -47,14 +50,37 @@ func authenticateCmd() *cobra.Command {
 				return err
 			}
 			if v, ok := c.Remote[args[0]]; ok {
-				_, _, err = getCredentials(cmd, cs, v.URL)
+				return getCredentials(cmd, cs, v.URL)
 			} else {
-				_, _, err = getCredentials(cmd, cs, args[0])
+				return getCredentials(cmd, cs, args[0])
 			}
-			return err
 		},
 	}
 	return cmd
+}
+
+func detectAuthType(cmd *cobra.Command, uriStr string) (authType conf.AuthType, err error) {
+	r, err := http.NewRequest(http.MethodPost, uriStr+"/oauth2/devicecode/", nil)
+	if err != nil {
+		return
+	}
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusBadRequest {
+		cmd.Printf("Detected auth type %q\n", conf.ATOauth2)
+		return conf.ATOauth2, nil
+	} else if resp.StatusCode == http.StatusNotFound {
+		cmd.Printf("Detected auth type %q\n", conf.ATLegacy)
+		return conf.ATLegacy, nil
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	return "", fmt.Errorf("unaticipated response from %s/oauth2/devicecode/ - %d: %s", uriStr, resp.StatusCode, string(b))
 }
 
 type AuthenticateRequest struct {
@@ -99,32 +125,130 @@ func authenticate(uri, email, password string) (token string, err error) {
 	return ar.IDToken, nil
 }
 
-func getCredentials(cmd *cobra.Command, cs *credentials.Store, uriS string) (uri *url.URL, token string, err error) {
-	u, err := url.Parse(uriS)
-	if err != nil {
-		return
-	}
+func getLegacyCredentials(cmd *cobra.Command, uriS string) (token string, err error) {
 	cmd.Printf("Enter your email and password for %s.\n", uriS)
 	reader := bufio.NewReader(cmd.InOrStdin())
 	cmd.Print("Email: ")
 	email, err := reader.ReadString('\n')
 	if err != nil {
-		return nil, "", err
+		return "", err
 	}
 	email = strings.TrimSpace(email)
 	password, err := utils.PromptForPassword(cmd)
 	if err != nil {
-		return nil, "", err
+		return "", err
 	}
 	token, err = authenticate(uriS, email, password)
 	if err != nil {
-		return nil, "", err
+		return "", err
+	}
+	return
+}
+
+func postForm(path string, form url.Values, respData interface{}) (err error) {
+	r, err := http.NewRequest(http.MethodPost, path, strings.NewReader(form.Encode()))
+	if err != nil {
+		return
+	}
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode >= 300 {
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("%d %s from %s: %s", resp.StatusCode, resp.Status, path, string(b))
+	}
+	if ct := resp.Header.Get("Content-Type"); !strings.Contains(ct, "application/json") {
+		return fmt.Errorf("unrecognized content type: %q", ct)
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	return json.Unmarshal(b, respData)
+}
+
+type DeviceCodeResponse struct {
+	DeviceCode      string `json:"device_code"`
+	UserCode        string `json:"user_code"`
+	VerificationURI string `json:"verification_uri"`
+	ExpiresIn       int    `json:"expires_in"`
+	Interval        int    `json:"interval"`
+}
+
+type TokenResponse struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+}
+
+func getOauth2Credentials(cmd *cobra.Command, uriS string) (token string, err error) {
+	reader := bufio.NewReader(cmd.InOrStdin())
+	cmd.Printf("Enter OAuth 2 client ID for this CLI: ")
+	clientID, err := reader.ReadString('\n')
+	if err != nil {
+		return
+	}
+	clientID = strings.TrimSpace(clientID)
+
+	form := url.Values{}
+	form.Set("client_id", clientID)
+	dcResp := &DeviceCodeResponse{}
+	if err = postForm(uriS+"/oauth2/devicecode/", form, dcResp); err != nil {
+		return
+	}
+
+	cmd.Printf("Visit %s/oauth2/device/ in your browser and enter user code %q to login\n", uriS, dcResp.UserCode)
+
+	form = url.Values{}
+	form.Set("client_id", clientID)
+	form.Set("grant_type", "urn:ietf:params:oauth:grant-type:device_code")
+	form.Set("device_code", dcResp.DeviceCode)
+	tokResp := &TokenResponse{}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(dcResp.ExpiresIn)*time.Second)
+	defer cancel()
+	ticker := time.NewTicker(time.Duration(dcResp.Interval) * time.Second)
+	for {
+		select {
+		case <-ctx.Done():
+			err = fmt.Errorf("login timeout. Last error: %v", err)
+			return
+		case <-ticker.C:
+			if err = postForm(uriS+"/oauth2/token/", form, tokResp); err == nil {
+				cmd.Printf("")
+				return tokResp.AccessToken, nil
+			}
+		}
+	}
+}
+
+func getCredentials(cmd *cobra.Command, cs *credentials.Store, uriS string) (err error) {
+	uriS = strings.TrimRight(uriS, "/")
+	u, err := url.Parse(uriS)
+	if err != nil {
+		return
+	}
+	at, err := detectAuthType(cmd, uriS)
+	if err != nil {
+		return
+	}
+	var token string
+	if at == conf.ATLegacy {
+		token, err = getLegacyCredentials(cmd, uriS)
+	} else {
+		token, err = getOauth2Credentials(cmd, uriS)
+	}
+	if err != nil {
+		return
 	}
 	cs.Set(*u, token)
-	if err := cs.Flush(); err != nil {
-		return nil, "", err
+	if err = cs.Flush(); err != nil {
+		return
 	}
 	cmd.Printf("Saved credentials to %s\n", cs.Path())
-	uri = u
-	return
+	return nil
 }
