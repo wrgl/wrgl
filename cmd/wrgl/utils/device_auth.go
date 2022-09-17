ca623c209c326ab51f4996b6e6d1517886bf43a5
Khoi Pham <pckhoi@gmail.com>
2022-09-17 17:56:57 +0700

    refactor: remove print statements

--- cmd/wrgl/utils/device_auth.go
+++ cmd/wrgl/utils/device_auth.go
@@ -107,8 +107,6 @@ func (s *deviceFlowAuthServer) RequestRPT(cmd *cobra.Command, accessToken, clien
 	if err != nil {
 		return "", err
 	}
-	fmt.Printf("ticket: %s\n", s.ticket)
-	fmt.Printf("oldRPT: %s\n", oldRPT)
 	return kc.RequestRPT(accessToken, rp.RPTRequest{
 		Ticket: s.ticket,
 		RPT:    oldRPT,
